2e861214e634bf96076ead22858657dcda17e5d5
kharelashim <kharelashimssnc@gmail.com>
2023-09-07 16:10:26 +0545

    Added sign in signout and signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :friends
   #'home/index'
   root 'home#index'
   get 'home/about'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
