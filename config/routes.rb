0d475c40bce5d57bfdfb138bc02900b3a6de9e03
kharelashim <kharelashimssnc@gmail.com>
2023-09-08 14:41:21 +0545

    changed the homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :friends
-  #'home/index'
-  root 'home#index'
+  #get'home/index'
+  #root 'home#index'
+  root 'friends#index'
   get 'home/about'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
