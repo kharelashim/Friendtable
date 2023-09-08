ee707255e8d9a9aa36e1dcf7b6215394164845b6
kharelashim <kharelashimssnc@gmail.com>
2023-09-08 14:22:09 +0545

    after linking database, we made out website to show only the friend list of particular user not of all user in our friend list table

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,5 +1,7 @@
 class FriendsController < ApplicationController
   before_action :set_friend, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, except:  [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   # GET /friends or /friends.json
   def index
@@ -12,16 +14,19 @@ class FriendsController < ApplicationController
 
   # GET /friends/new
   def new
-    @friend = Friend.new
+    #friend = Friend.new
+    @friend = current_user.friend.build
   end
 
-  # GET /friends/1/edit
+  # GET /friends/1/ed it
   def edit
+
   end
 
   # POST /friends or /friends.json
   def create
-    @friend = Friend.new(friend_params)
+    #friend = Friend.new(friend_params)
+    @friend = current_user.friend.build(friend_params)
 
     respond_to do |format|
       if @friend.save
@@ -56,6 +61,11 @@ class FriendsController < ApplicationController
       format.json { head :no_content }
     end
   end
+
+    def correct_user
+      @friend = current_user.friend.find_by(id: params[:id])
+      redirect_to friends_path, notice: "Not authorized to edit this friend" if @friend.nil?
+    end 
 
   private
     # Use callbacks to share common setup or constraints between actions.
